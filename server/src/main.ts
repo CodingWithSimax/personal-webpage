00a4e9c15a5c18d0dc013d8df905c99cf04afdd0
Simax <simaxme@gmail.com>
2021-08-03 19:53:56 +0200

    added page parts

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ const PORT = 3000;
 const app = express();
 
 app.use("/", express.static("./web/dist"));
+app.use("/assets", express.static("./web/src/assets"));
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.sendFile("./web/dist/index.html");
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SideCircleModule } from './side-circle/side-circle.module';
+import { HomeModule } from './page-parts/home/home.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SideCircleModule],
+    imports: [BrowserModule, SideCircleModule, HomeModule],
     providers: [],
     bootstrap: [AppComponent],
 })
