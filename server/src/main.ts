947e9b42b45dc94206aa75e3dc02d4ad1fccf756
CodingWithSimax <simaxme@gmail.com>
2021-09-22 20:20:52 +0200

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
 import express = require("express");
 import fs = require("fs");
+import path = require("path");
 
 import { setupTelegramAPI } from "./telegram-api";
 import { setupPorts } from "./port-redirects";
 import { Config } from "./config-interface";
 import { ExpressLogger } from "./utils/setupLogger";
-
 
 // --- //
 const PORT = 3000;
@@ -29,19 +29,30 @@ expressLogger.addExpressApp(app, "main-web", false);
 app.use("/", express.static("./web/dist"));
 app.use("/assets", express.static("./web/src/assets"));
 
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.sendFile("./web/dist/index.html");
-});
+const handleAngularRequest = (req: express.Request, res: express.Response) => {
+    res.sendFile(path.resolve("./web/dist/index.html"));
+};
+app.get("/", handleAngularRequest);
+app.get("/error", handleAngularRequest);
 
 setupTelegramAPI(app);
 
+for (const portRedirect of CONFIG.portRedirects) {
+    for (const prefix of portRedirect.prefixes) {
+        if (prefix.startsWith("/")) {
+            app.get(prefix, (req: express.Request, res: express.Response) => {
+                res.send(portRedirect.url);
+            });
+        }
+    }
+}
+
 expressLogger.addExpressApp(app, "main-web", true);
 app.use((req: express.Request, res: express.Response) => {
-    res.json({error: "redirect expected"});
+    res.redirect("/error");
 });
 
 app.listen(PORT, () => {
     console.log(`Now listening to port ${PORT}`);
     console.log("started server");
 });
-
